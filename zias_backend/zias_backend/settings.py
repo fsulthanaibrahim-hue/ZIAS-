8bc00260e1615d463a515990a4c18332c066c97b
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-07-27 17:53:13 +0530

    settings update

--- zias_backend/zias_backend/settings.py
+++ zias_backend/zias_backend/settings.py
@@ -27,8 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-DEBUG = True
-
 ALLOWED_HOSTS = [
     "*"
 ]
